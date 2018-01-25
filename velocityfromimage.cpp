9f5b72141c764b768791ed6c086de2a30a8b7b51
melvinjames7 <melvinjames7@gmail.com>
2018-01-25 00:06:32 -0600

    finished

--- velocityfromimage.cpp
+++ velocityfromimage.cpp
@@ -17,6 +17,118 @@ using namespace std;
 Point Purple1,Purple2, Orange1, Orange2;
 int flag = 0,Pradius=0,Oradius=0;
 
+Point get3Dcordinates(int x, int y, int z)
+{
+    
+    Mat inverse=(Mat_<double>(3,3)<<
+                 0.0027289,0,-0,
+                 -7.1662e-06,0.0027186,-0,
+                 -0.71173,-0.56546,1);
+    
+    Mat depth, m;
+    
+    depth=(Mat_<double>(1, 3)<<y, x, 1);
+    
+    m = depth * inverse;
+    m = m.mul(z);
+    
+    double x1 = m.at<double>(0,0);
+    double y1 = m.at<double>(0,1);
+    
+    return Point(x1,y1);
+}
+
+void RelativeVelocity(Mat image1, Mat image2)
+{
+    Point Purple1_3D,Purple2_3D,Orange1_3D,Orange2_3D;
+    float PurpleV,RedV, z1,z2;
+    
+    
+    z1 = image1.at<ushort>(Purple1.y,Purple1.x);
+    Purple1_3D = get3Dcordinates(Purple1.x,Purple1.y,z1);
+    
+    z2 = image2.at<ushort>(Purple2.y,Purple2.x);
+    Purple2_3D = get3Dcordinates(Purple2.x,Purple2.y,z2);
+    
+    PurpleV = sqrt(powf((Purple2_3D.x - Purple1_3D.x),2)+powf((Purple2_3D.y - Purple1_3D.y),2)+
+                   powf((z2- z1),2));
+    
+    
+    z1 = image1.at<ushort>(Orange1.y,Orange1.x);
+    Orange1_3D = get3Dcordinates(Orange1.x,Orange1.y,z1);
+    
+    
+    z2 = image2.at<ushort>(Orange2.y,Orange2.x);
+    
+    
+    Orange2_3D = get3Dcordinates(Orange2.x,Orange2.y,z2);
+    
+    
+    RedV = sqrt(powf((Orange2_3D.x - Orange1_3D.x),2)+powf((Orange2_3D.y - Orange1_3D.y),2)+ powf((z2- z1),2));
+    
+    float mmPs =  ((PurpleV+RedV)/2433)*1000.0;
+    cout<<"Relative Velocity = "<<mmPs<<"mm/s";
+}
+
+Mat colorize(Mat image1 , Mat color1)
+{
+    Mat image = image1.clone();//depth image
+    Mat color = color1.clone();//corresponding color image
+    
+    Mat inverse=(Mat_<double>(3,3)<<
+                 0.0027289,0,-0,
+                 -7.1662e-06,0.0027186,-0,
+                 -0.71173,-0.56546,1);
+    Mat D2C=(Mat_<double>(4,4)<<
+             0.99998, -0.0062464, 0.0013162, 0,
+             0.0062361, 0.99997, 0.0046386, 0,
+             -0.0013491, -0.0046356, 0.99999, 0,
+             50.775, 11.994, -80.412, 1);
+    Mat RGB=(Mat_<double>(3,3)<<
+             1027, 0, 0,
+             3.4052, 1029.9, 0,
+             968, 536.54, 1);
+    
+    
+    Mat depth;
+    
+    
+    Mat new_image1 = Mat::zeros( image.size(), color.type());
+    Mat m,m2,m3,m4;
+    for( int x = 0; x < image.rows; x++ )
+    {
+        for( int y = 0; y < image.cols; y++ )
+        {
+            depth=(Mat_<double>(1, 3)<<y, x, 1);
+            int z_coord = image.at<ushort>( x,y);
+            if(z_coord==0)
+                continue;
+            m = depth * inverse;
+            m = m.mul(z_coord);
+            
+            
+            m2=(Mat_<double>(1, 4)<<
+                m.at<double>(0, 0),m.at<double>(0,1),m.at<double>(0,2),1);
+            m3 = m2 * D2C;
+            
+            m4 = (Mat_<double>(1, 3)<<
+                  m3.at<double>(0,0)/m3.at<double>(0,2) , m3.at<double>(0,1)/m3.at<double>(0,2) , 1);
+            m4*=RGB;
+            
+            double x1 = m4.at<double>(0,0);
+            double y1 = m4.at<double>(0,1);
+            if(x1>=0 && x1<color.cols && y1>=0 && y1< color.rows)
+            {
+                new_image1.at<Vec3b>(x,y)[0] = saturate_cast<uchar>( color.at<Vec3b>(y1,x1)[0]);
+                new_image1.at<Vec3b>(x,y)[1] = saturate_cast<uchar>( color.at<Vec3b>(y1,x1)[1]);
+                new_image1.at<Vec3b>(x,y)[2] = saturate_cast<uchar>( color.at<Vec3b>(y1,x1)[2]);
+            }
+        }
+    }
+    
+    return new_image1;
+}
+
 Mat makeBinary(Mat image)
 {
     Mat d_image = Mat::zeros( image.size(), image.type() );  //for new brightness adjusted image
@@ -106,6 +218,7 @@ Mat makeBinary(Mat image)
     rectangle(d_image, Point(X2-width2/2,Y2-width2/2), Point(X2+width2/2,Y2+width2/2), Scalar(0,0,255));
     return d_image;
 }
+
 
 int main(int argc, const char * argv[]) {
     string CimageName1("/Users/melvinjames/Desktop/color_92331.png");
